23a500cb982680abd402019bd459974f78e2d079
Stavya Datta <stavyad@student.unimelb.edu.au>
2022-04-18 11:19:40 +0530

    Added image augmentation

--- imageAugmentation.py
+++ imageAugmentation.py
@@ -1,1 +1,29 @@
-m 
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import img_to_array
+from imutils import paths
+import random
+import cv2
+import os
+
+datagen = ImageDataGenerator()
+
+# creating X and y labels
+imagePaths = sorted(list(paths.list_images("Dataset")))
+
+for imagePath in imagePaths:
+    label = imagePath.split(os.path.sep)[-2]
+    image = cv2.imread(imagePath)
+    image = img_to_array(image)
+
+X, y = ...
+
+for imagePath in imagePaths:
+    
+
+
+random.shuffle(imagePaths)
+
+it = datagen.flow(X, y)
+
+image = cv2.imread(imagePath)
+
